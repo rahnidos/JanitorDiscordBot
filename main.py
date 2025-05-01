8ee48e5acce54ab0844ee41cbf0d4f985341410a
Michal Franczak <rahnidos@stricte.net>
2025-05-01 17:31:25 +0200

    goLive

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import json
+import asyncio
+from datetime import datetime, timedelta, timezone
 
 
 load_dotenv()
@@ -20,7 +22,36 @@ messages = load_language(LANGUAGE)
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+class MyBot(commands.Bot):
+    async def setup_hook(self):
+        self.loop.create_task(schedule_daily_task())
+
+bot = MyBot(command_prefix='!', intents=intents)
+
+async def delete_old_images():
+    await bot.wait_until_ready()
+    channels = json.loads(os.getenv("CHANNELS", "[]"))  
+    three_days_ago = datetime.now(timezone.utc) - timedelta(days=3)
+
+    for channel_id in channels:
+        channel = bot.get_channel(int(channel_id))
+        if channel:
+            async for message in channel.history(limit=None):
+                if message.attachments and message.created_at < three_days_ago:
+                    try:
+                        await message.delete()
+                        await asyncio.sleep(1)  
+                    except discord.errors.HTTPException as e:
+                        print(f"Can't delete message {e}")
+
+async def schedule_daily_task():
+    while True:
+        now = datetime.now()
+        target_time = now.replace(hour=12, minute=0, second=0, microsecond=0)
+        if now > target_time:
+            target_time += timedelta(days=1)
+        await asyncio.sleep((target_time - now).total_seconds())
+        await delete_old_images()
 
 @bot.event
 async def on_ready():
@@ -29,5 +60,10 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send(messages["hello_message"])
+
+@bot.command()
+async def clean(ctx):
+    await delete_old_images() 
+    await ctx.send(messages["clean_over"])
 
 bot.run(TOKEN)
